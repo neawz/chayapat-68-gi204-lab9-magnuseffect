1e36c8234536d10e1eeadc232a2ba1722a421cf1
neawz <chayapat2549@gmail.com>
2026-03-10 10:01:36 +0700

    Angular Velocity
    + Football Field, Football Texture

---
+++ Assets/Scripts/TorqueRotate.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class TorqueRotate : MonoBehaviour
+{
+    public int torquePower;
+    Rigidbody rb;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Keyboard.current.dKey.isPressed)
+        {
+            rb.AddTorque(0, 0, torquePower);
+        }
+    }
+}
